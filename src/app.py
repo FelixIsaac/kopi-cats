9fc5703a3d46e52b7deee43fbfd2faf628b83b1f
Icesotope <53615543+AbsoluteZeroist@users.noreply.github.com>
2021-07-03 02:04:02 +0800

    creating display window.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,21 @@
 import pygame
 
 def main():
-    return
+     
+    pygame.init()
+    
+    pygame.display.set_logo()
+    pygame.display.set_caption("Kopi-Cat Uno")
+
+    screen = pygame.display.set_mode((720,480))
+
+    isRunning = True
+    while isRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                isRunning = False 
+                pygame.dispay.quit()
+
+
+if __name__ == "__main__":
+    main()    
